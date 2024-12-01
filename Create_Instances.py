422ba68424fc404b02f5d81a1091cf436a451be9
krijen1144 <krijen15887@stud.noroff.no>
2024-12-01 11:45:08 +0100

    Add files via upload

---
+++ Create_Instances.py
@@ -0,0 +1,8 @@
+from UserInputValidator import UserInputValidator
+
+val1 = UserInputValidator()
+val2 = UserInputValidator()
+
+input_1 = ["12", "20", "testing", "98", "not_a_int"]
+input_2 = [12, 20, "testing", 91, "not_a_int", 21, 23,45]
+

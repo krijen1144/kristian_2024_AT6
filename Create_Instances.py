060c8b549be9cce7d67b29ae8968d263aec3fae8
Kristianhaljen <kristian.jensen@fraktal.no>
2024-12-01 11:58:33 +0100

    Update Create_Instances.py

--- Create_Instances.py
+++ Create_Instances.py
@@ -6,3 +6,8 @@ val2 = UserInputValidator()
 input_1 = ["12", "20", "testing", "98", "not_a_int"]
 input_2 = [12, 20, "testing", 91, "not_a_int", 21, 23,45]
 
+valid_num_1 = val1.validate_positive_integers(input_1)
+valid_num_2 = val2.validate_positive_integers(input_2)
+
+print("Valid numbers in list 1: ", valid_num_1)
+print("Valid numbers in list 2: ", valid_num_2)

e3d0a0acd799388d54caea55225661e98512d917
Kristianhaljen <kristian.jensen@fraktal.no>
2024-12-01 12:08:59 +0100

    Update UserInputValidator.py

--- UserInputValidator.py
+++ UserInputValidator.py
@@ -2,12 +2,12 @@ class UserInputValidator:
     def __init__(self):
         pass
 
-def validate_positive_integers(self, input_list):
-    valid_int = []
-    for item in input_list:
-        if item.isdigit() and int(item)>0:
-            valid_int.append(int(item))
-    return valid_int
+    def validate_positive_integers(self, input_list):
+        valid_int = []
+        for item in input_list:
+            if item.isdigit() and int(item)>0:
+                valid_int.append(int(item))
+        return valid_int
 
-def display(self):
-    print("Validation Complete")
+    def display(self):
+        print("Validation Complete")

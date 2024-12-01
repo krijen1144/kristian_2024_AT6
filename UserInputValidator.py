84927c0cabc4ba0be4d499d357e01e4650fe39d9
krijen1144 <krijen15887@stud.noroff.no>
2024-12-01 11:35:28 +0100

    Add files via upload

---
+++ UserInputValidator.py
@@ -0,0 +1,11 @@
+class UserInputValidator:
+    def __init__(self):
+        pass
+
+def validate_positive_integers(self, input_list):
+    valid_int = []
+    for item in input_list:
+        if item.isdigit() and int(item)>0:
+            valid_int.append(int(item))
+    return valid_int
+
